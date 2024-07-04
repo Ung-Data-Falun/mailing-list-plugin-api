9da571b850096f3bc1c25ffb8e11bc9a95dd9e14
vanten-s <vanten-s@vanten-s.com>
2024-07-04 14:30:26 +0200

    V0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,21 @@
+use std::os::raw::c_char;
+
 macro_rules! pluginFn {
     ( $o:ty, $( $x:ty ),* ) => {
-         Option<*mut extern "C" fn($($x,)*) -> $o>
+         Optional<unsafe extern "C" fn($($x,)*) -> $o>
     };
 }
 
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
+pub enum Optional<T> {
+    Some(T),
+    None,
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
 pub struct Plugin {
-    pub message_handler: pluginFn!((), String),
+    pub message_handler: pluginFn!((), *const c_char),
 }
 
